d25659012e7fdcd22c85d464973057e1fbeb8ffa
Braa <Braa@DESKTOP-J6639GF>
2024-04-28 18:51:58 +0300

    Add project files.

---
+++ FirstTask/Controllers/RegisterAndLoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTask.Controllers
+{
+    public class RegisterAndLoginController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
